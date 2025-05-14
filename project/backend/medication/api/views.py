12352f0e412d4d8e6e45e75bcde4ee25bc3801ff
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-05-14 19:06:36 +0300

    Add Take pill backend

--- project/backend/medication/api/views.py
+++ project/backend/medication/api/views.py
@@ -1,3 +1,6 @@
+import datetime
+from django.utils.timezone import localdate, localtime
+
 from rest_framework import generics, permissions
 from medication.models import Medication, MedicationSchedule, MedicationLog, DailyNote
 from .serializers import (
@@ -50,7 +53,15 @@ class MedicationLogListCreateView(generics.ListCreateAPIView):
         return MedicationLog.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        data = serializer.validated_data
+        # medication_name
+        serializer.save(
+            user=self.request.user,
+            dose_time=data.get('dose_time'),
+            medication_name=data.get('medication').name,
+            date=localdate(),
+            time=localtime()
+        )
 
 class MedicationLogDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MedicationLogSerializer
@@ -58,6 +69,7 @@ class MedicationLogDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return MedicationLog.objects.filter(user=self.request.user)
+
 
 # DailyNote Views
 class DailyNoteListCreateView(generics.ListCreateAPIView):
@@ -87,5 +99,8 @@ class TodayMedicationAPIView(ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = SimpleMedicationScheduleSerializer
 
+
     def get_queryset(self, **kwargs):
-        return MedicationTimeService.for_user_today(self.request.user)
+        meds = MedicationTimeService.for_user_today(self.request.user)
+        sorted_data = sorted(meds, key=lambda x: x['time'])
+        return sorted_data
