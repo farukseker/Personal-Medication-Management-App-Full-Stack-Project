3bf8ab390e3938f05325bfb8f36718e8d6792d95
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-05-15 16:38:31 +0300

    Add WeeklyDosePlan model

---
+++ project/backend/medication/api/views/medication_list_create_view.py
@@ -0,0 +1,15 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from medication.api.serializers import MedicationSerializer
+from medication.models import Medication
+
+
+class MedicationListCreateView(ListCreateAPIView):
+    serializer_class = MedicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Medication.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
