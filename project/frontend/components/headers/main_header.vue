90fe8f6486b2da58004a1b6557e3d4e905f4f050
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-07-10 23:05:39 +0300

    Add header

---
+++ project/frontend/components/headers/main_header.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="flex flex-col gap-4 flex-1">
+    <div class="flex justify-between items-center">
+      <h1 class="text-xl font-bold">{{ title }}</h1>
+      <button 
+        @click="go('/settings')"
+        class="btn btn-ghost btn-circle text-2xl p-0">
+        <img class="w-[36px] h-[36px] object-cover rounded-full shadow-md" src="/pp.jpg" alt="">
+      </button>
+    </div>
+  </div>
+  <div class="flex w-full my-2">
+    <h2 class="text-lg font-semibold w-full">
+      <TodayMessage />
+    </h2>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { useLocaleRouter } from '~/composables/useLocaleRouter'
+const { go } = useLocaleRouter()
+defineProps({
+    title: {
+        Object: String,
+        required: false,
+        default: 'MediMind'
+    }
+})
+</script>
